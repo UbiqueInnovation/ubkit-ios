5e5cc83f1a22438b856bf00f9d4f8d94e4997dc1
Joseph El Mallah <elmallah@ubique.ch>
2019-03-20 11:01:19 +0100

    Formatted the HTTP Code class

--- Sources/Networking/HTTPCode.swift
+++ Sources/Networking/HTTPCode.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 // MARK: - HTTP code integer
+
 extension Int {
     /// Get the http category
     var httpCodeCategory: HTTPCodeCategory {
         return HTTPCodeCategory(code: self)
     }
-    
+
     /// Get the standard http code
     var standardHTTPCode: StandardHTTPCode? {
-        return StandardHTTPCode.init(rawValue: self)
+        return StandardHTTPCode(rawValue: self)
     }
 }
-
 
 /// A cluster of HTTP code
 public enum HTTPCodeCategory {
@@ -35,21 +35,21 @@ public enum HTTPCodeCategory {
     case clientError
     /// Server Error Status
     case serverError
-    
+
     /// Initializes an HTTP code category
     ///
     /// - Parameter code: The HTTP code
     public init(code: Int) {
         switch code {
-        case 100..<200:
+        case 100 ..< 200:
             self = .informational
-        case 200..<300:
+        case 200 ..< 300:
             self = .success
-        case 300..<400:
+        case 300 ..< 400:
             self = .redirection
-        case 400..<500:
+        case 400 ..< 500:
             self = .clientError
-        case 500..<600:
+        case 500 ..< 600:
             self = .serverError
         default:
             self = .uncategorized
@@ -59,23 +59,23 @@ public enum HTTPCodeCategory {
 
 /// The standard HTTP codes
 public enum StandardHTTPCode: Int {
-    
+
     // MARK: - 2xx Success
-    
+
     /// The request has succeeded.
     case OK = 200
     /// The request has been fulfilled and resulted in a new resource being created.
     case created = 201
     /// The server has fulfilled the request but does not need to return an entity-body, and might want to return updated metainformation.
     case noContent = 204
-    
+
     // MARK: - 3xx Redirection
-    
+
     /// If the client has performed a conditional GET request and access is allowed, but the document has not been modified.
     case notModified = 304
-    
+
     // MARK: - 4xx Client Error
-    
+
     /// The request could not be understood by the server due to malformed syntax.
     case badRequest = 400
     /// The request requires user authentication.
@@ -86,9 +86,9 @@ public enum StandardHTTPCode: Int {
     case notFound = 404
     /// The request could not be completed due to a conflict with the current state of the resource.
     case conflict = 409
-    
+
     // MARK: - 5xx Server Error
-    
+
     /// The server encountered an unexpected condition which prevented it from fulfilling the request.
     case internalServerError = 500
     /// The server does not support the functionality required to fulfill the request.
