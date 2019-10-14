9d9fc198fb3f52ba175ababdb53cc1182330fb39
Marco Zimmermann <zimmermann@ubique.ch>
2019-10-14 20:19:45 +0200

    UBF: fixes naming

--- Sources/UI Elements/UIImage/UIImage+Helpers.swift
+++ Sources/UI Elements/UIImage/UIImage+Helpers.swift
@@ -11,8 +11,8 @@ import UIKit
 
 extension UIImage
 {
-    /// Initializes an image with a constant color
-    public static func ub_image(with color: UIColor) -> UIImage?
+    /// Initializes an image with a single pixel with a constant color
+    public static func ub_singlePixelImage(with color: UIColor) -> UIImage?
     {
         let rect = CGRect(x: 0.0, y: 0.0, width: 1.0, height: 1.0)
         UIGraphicsBeginImageContext(rect.size)
