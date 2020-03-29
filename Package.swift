3f45624e16e1fd095f4bbb5791b36522312e6cd4
Nicolas Märki <maerki@ubique.ch>
2020-03-29 19:13:44 +0200

    NS: Moved Push to new repository

--- Package.swift
+++ Package.swift
@@ -11,13 +11,10 @@ let package = Package(
     ],
     products: [
         .library(name: "UBFoundation", targets: ["UBFoundation"]),
-        .library(name: "UBFoundationPush", targets: ["UBFoundationPush"]),
-        .library(name: "UBFoundationLocation", targets: ["UBFoundationLocation"])
+
     ],
     targets: [
         .target(name: "UBFoundation", path: "Sources"),
-        .target(name: "UBFoundationPush", dependencies: ["UBFoundation"], path: "UBFoundationPush/Sources"),
-        .target(name: "UBFoundationLocation", dependencies: ["UBFoundation"], path: "UBFoundationLocation/Sources"),
         .testTarget(name: "Tests", dependencies: ["UBFoundation"], path: "Tests"),
         .testTarget(name: "LocationTests", dependencies: ["UBFoundation", "UBFoundationLocation"], path: "UBFoundationLocation/Tests")
     ]
