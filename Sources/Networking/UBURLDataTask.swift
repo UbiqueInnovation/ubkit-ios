2bec779be403bcade896d6d64d43c9b46f0d829b
Nicolas Märki <maerki@ubique.ch>
2020-07-17 19:46:41 +0200

    synchronousStartSemaphore.signal in deinit

--- Sources/Networking/UBURLDataTask.swift
+++ Sources/Networking/UBURLDataTask.swift
@@ -100,6 +100,7 @@ public final class UBURLDataTask: UBURLSessionTask, CustomStringConvertible, Cus
         dataTaskStateObservation = nil
         dataTask?.cancel()
         requestStartSemaphore.signal()
+        synchronousStartSemaphore.signal()
     }
 
     // MARK: - Startin and stopping
