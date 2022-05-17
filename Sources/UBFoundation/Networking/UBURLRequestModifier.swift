746441545df9adc4a7c4d2fcfcf4eab884ab19e3
Nicolas Märki <maerki@users.noreply.github.com>
2022-05-17 20:03:15 +0200

    change async datatask api

--- Sources/UBFoundation/Networking/UBURLRequestModifier.swift
+++ Sources/UBFoundation/Networking/UBURLRequestModifier.swift
@@ -16,3 +16,33 @@ public protocol UBURLRequestModifier {
     ///   - completion: The completion handler to be called when the modification are finished.
     func modifyRequest(_ originalRequest: UBURLRequest, completion: @escaping (Result<UBURLRequest, Error>) -> Void)
 }
+
+/// A request modifier is called before a HTTPDataTask starts and have the chance to change the request.
+@available(iOS 13.0, *)
+public protocol UBAsyncURLRequestModifier: UBURLRequestModifier {
+    /// Modifies the request before it will start.
+    ///
+    /// - Parameters:
+    ///   - originalRequest: The original request before modification or the most recent request updated by the previous modifiers.
+    ///   - completion: The completion handler to be called when the modification are finished.
+    func modifyRequest(_ originalRequest: UBURLRequest) async throws -> UBURLRequest
+
+
+}
+
+@available(iOS 13.0, *)
+extension UBAsyncURLRequestModifier {
+    func modifyRequest(_ originalRequest: UBURLRequest, completion: @escaping (Result<UBURLRequest, Error>) -> Void) {
+        Task {
+            do {
+                let newRequest = try await modifyRequest(originalRequest)
+                completion(.success(newRequest))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+    }
+}
+
+
