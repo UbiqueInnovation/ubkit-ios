62987bf52662f6380764578b18ff973844208784
Nicolas Märki <maerki@users.noreply.github.com>
2022-05-19 10:50:39 +0200

    add error decoding to async apis

--- Sources/UBFoundation/Networking/Concurrency/UBURLDataTask+Concurrency.swift
+++ Sources/UBFoundation/Networking/Concurrency/UBURLDataTask+Concurrency.swift
@@ -36,6 +36,26 @@ public extension UBURLDataTask {
         }
     }
 
+    func loadOnce<T, E: UBURLDataTaskErrorBody>(decoder: UBURLDataTaskDecoder<T>, errorDecoder: UBURLDataTaskDecoder<E>) async throws -> (result: T, meta: MetaData) {
+        return try await withCheckedThrowingContinuation { cont in
+
+            var id: UUID?
+
+            id = self.addCompletionHandler(decoder: decoder, errorDecoder: errorDecoder, callbackQueue: Self.concurrencyCallbackQueue) { result, response, info, task in
+                switch result {
+                case let .success(res):
+                    cont.resume(returning: (result: res, meta: MetaData(info: info, response: response)))
+                case let .failure(e):
+                    cont.resume(throwing: e)
+                }
+                if let id = id {
+                    self.removeCompletionHandler(identifier: id)
+                }
+            }
+            self.start()
+        }
+    }
+
     func loadOnce() async throws -> (Data, MetaData) {
         try await self.loadOnce(decoder: UBDataPassthroughDecoder())
     }
@@ -43,6 +63,28 @@ public extension UBURLDataTask {
     func startCronStream<T>(decoder: UBURLDataTaskDecoder<T>) -> AsyncThrowingStream<(T, MetaData), Error> {
         AsyncThrowingStream { cont in
             let id = self.addCompletionHandler(decoder: decoder, callbackQueue: Self.concurrencyCallbackQueue) { result, response, info, task in
+                switch result {
+                case let .success(res):
+                    cont.yield((res, MetaData(info: info, response: response)))
+                case let .failure(e):
+                    cont.finish(throwing: e)
+                }
+            }
+
+            cont.onTermination = { @Sendable [weak self] _ in
+                self?.cancel()
+                self?.removeCompletionHandler(identifier: id)
+            }
+            self.cleanupBeforeDeinit = {
+                cont.finish()
+            }
+            self.start()
+        }
+    }
+
+    func startCronStream<T, E: UBURLDataTaskErrorBody>(decoder: UBURLDataTaskDecoder<T>, errorDecoder: UBURLDataTaskDecoder<E>) -> AsyncThrowingStream<(T, MetaData), Error> {
+        AsyncThrowingStream { cont in
+            let id = self.addCompletionHandler(decoder: decoder, errorDecoder: errorDecoder, callbackQueue: Self.concurrencyCallbackQueue) { result, response, info, task in
                 switch result {
                 case let .success(res):
                     cont.yield((res, MetaData(info: info, response: response)))
