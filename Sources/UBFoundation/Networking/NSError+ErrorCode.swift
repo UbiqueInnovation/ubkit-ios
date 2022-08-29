6f4b2cfc1232fe75d8cc5ea74e7d0de349252f59
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2022-08-29 08:02:45 +0200

    swiftformat

--- Sources/UBFoundation/Networking/NSError+ErrorCode.swift
+++ Sources/UBFoundation/Networking/NSError+ErrorCode.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Patrick Amrein on 26.08.22.
 //
@@ -8,24 +8,24 @@
 import Foundation
 
 extension NSError {
-    public var errorCode : String {
+    public var errorCode: String {
         if let codedError = self as? UBCodedError {
             return codedError.errorCode
         } else {
             return "[\(self.mapDomain(self.domain))-\(self.code)]"
         }
     }
-    
-    func mapDomain(_ domain : String) -> String {
+
+    func mapDomain(_ domain: String) -> String {
         switch domain {
             case "kCFErrorDomainCFNetwork":
                 return "CFN"
             default:
-            if domain.contains("kCFErrorDomain") {
-                let domainPrefix =  domain.replacingOccurrences(of: "kCFErrorDomain", with: "").replacingOccurrences(of: "CF", with: "").prefix(1)
-                return "CF\(domainPrefix)"
-            }
-            return domain
+                if domain.contains("kCFErrorDomain") {
+                    let domainPrefix = domain.replacingOccurrences(of: "kCFErrorDomain", with: "").replacingOccurrences(of: "CF", with: "").prefix(1)
+                    return "CF\(domainPrefix)"
+                }
+                return domain
         }
     }
 }
