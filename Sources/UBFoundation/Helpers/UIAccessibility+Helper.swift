b485443ab4cb6462a8dab8f7a3497b45ffdb2239
Jan Huber <huber@ubique.ch>
2024-05-06 08:03:52 +0200

    Add postHighPriorityAnnouncement

---
+++ Sources/UBFoundation/Helpers/UIAccessibility+Helper.swift
@@ -0,0 +1,23 @@
+//
+//  UIAccessibility+Helper.swift
+//  UBFoundation
+//
+//  Created by Jan Huber on 06.05.2024.
+//
+
+import UIKit
+
+extension UIAccessibility {
+    /// Reads an announcement to VoiceOver users that cannot be interrupted by other announcements on devices running iOS 17 or never.
+    /// - Parameter message: the message to read
+    static func postHighPriorityAnnouncement(_ message: String) {
+        if #available(iOS 17, *) {
+            let announcement = NSAttributedString(
+                string: message,
+                attributes:[NSAttributedString.Key.accessibilitySpeechAnnouncementPriority: UIAccessibilityPriority.high])
+            AccessibilityNotification.Announcement(announcement).post()
+        } else {
+            UIAccessibility.post(notification: .announcement, argument: message)
+        }
+    }
+}
