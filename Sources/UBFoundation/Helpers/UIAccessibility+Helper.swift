58ff6bc1ed3c5d726b6c904f7a73b7325b82baf1
Nicolas Märki <maerki@users.noreply.github.com>
2024-05-07 15:26:29 +0200

    Swift Format

--- Sources/UBFoundation/Helpers/UIAccessibility+Helper.swift
+++ Sources/UBFoundation/Helpers/UIAccessibility+Helper.swift
@@ -7,14 +7,15 @@
 
 import UIKit
 
-extension UIAccessibility {
+public extension UIAccessibility {
     /// Reads an announcement to VoiceOver users that cannot be interrupted by other announcements on devices running iOS 17 or never.
     /// - Parameter message: the message to read
-    public static func ub_postHighPriorityAnnouncement(_ message: String) {
+    static func ub_postHighPriorityAnnouncement(_ message: String) {
         if #available(iOS 17, *) {
             let announcement = NSAttributedString(
                 string: message,
-                attributes:[NSAttributedString.Key.accessibilitySpeechAnnouncementPriority: UIAccessibilityPriority.high])
+                attributes: [NSAttributedString.Key.accessibilitySpeechAnnouncementPriority: UIAccessibilityPriority.high]
+            )
             AccessibilityNotification.Announcement(announcement).post()
         } else {
             UIAccessibility.post(notification: .announcement, argument: message)
