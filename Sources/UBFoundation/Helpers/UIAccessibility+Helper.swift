21973e4c2ad234a998b14952eff21396a4719a13
Jan Huber <huber@ubique.ch>
2024-05-10 11:53:25 +0200

    UIAccessibility-Extension: Fix watchOS builds

--- Sources/UBFoundation/Helpers/UIAccessibility+Helper.swift
+++ Sources/UBFoundation/Helpers/UIAccessibility+Helper.swift
@@ -11,13 +11,15 @@ extension UIAccessibility {
     /// Reads an announcement to VoiceOver users that cannot be interrupted by other announcements on devices running iOS 17 or never.
     /// - Parameter message: the message to read
     public static func ub_postHighPriorityAnnouncement(_ message: String) {
-        if #available(iOS 17, *) {
+        if #available(iOS 17, watchOS 10.0, *) {
             let announcement = NSAttributedString(
                 string: message,
                 attributes:[NSAttributedString.Key.accessibilitySpeechAnnouncementPriority: UIAccessibilityPriority.high])
             AccessibilityNotification.Announcement(announcement).post()
         } else {
+#if os(iOS)
             UIAccessibility.post(notification: .announcement, argument: message)
+#endif
         }
     }
 }
