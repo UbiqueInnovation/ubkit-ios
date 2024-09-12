9d56c81a7caacdb4077aff8573830e44d99f9fe0
Nicolas Märki <maerki@users.noreply.github.com>
2024-09-12 17:45:18 +0200

    fix availability

--- Sources/UBFoundation/Logging/LoggingMacros.swift
+++ Sources/UBFoundation/Logging/LoggingMacros.swift
@@ -8,35 +8,35 @@
 import Foundation
 import os
 
-@available(iOS 14.0, *)
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 @freestanding(expression)
 public macro print(_ message: OSLogMessage) = #externalMacro(
     module: "UBMacros",
     type: "UBPrintMacro"
 )
 
-@available(iOS 14.0, *)
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 @freestanding(expression)
 public macro printError(_ message: String) = #externalMacro(
     module: "UBMacros",
     type: "UBPrintErrorMacro"
 )
 
-@available(iOS 14.0, *)
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 @freestanding(expression)
 public macro assert(_ condition: Bool, _ message: @autoclosure () -> String = String()) = #externalMacro(
     module: "UBMacros",
     type: "UBAssertMacro"
 )
 
-@available(iOS 14.0, *)
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 @freestanding(expression)
 public macro assertionFailure(_ message: @autoclosure () -> String = String()) = #externalMacro(
     module: "UBMacros",
     type: "UBAssertionFailureMacro"
 )
 
-@available(iOS 14.0, *)
+@available(macOS 11.0, iOS 14.0, watchOS 7.0, tvOS 14.0, *)
 public struct UBPrintMacro {
     public static let Logger = os.Logger(subsystem: "UBLog", category: "Default")
 
