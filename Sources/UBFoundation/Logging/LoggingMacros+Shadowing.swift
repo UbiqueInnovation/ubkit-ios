7e0f8e7dc9f7e1d7fe9fb2c1611e68df7a33b3a4
Nicolas Märki <maerki@users.noreply.github.com>
2024-09-13 11:25:26 +0200

    deprecated

--- Sources/UBFoundation/Logging/LoggingMacros+Shadowing.swift
+++ Sources/UBFoundation/Logging/LoggingMacros+Shadowing.swift
@@ -5,17 +5,17 @@
 //  Created by Nicolas Märki on 12.09.2024.
 //
 
-@available(*, message: "Use #print instead")
+@available(*, deprecated, message: "Use #print instead")
 public func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {
     Swift.print(items, separator: separator, terminator: terminator)
 }
 
-@available(*, message: "Use #assert instead")
+@available(*, deprecated, message: "Use #assert instead")
 public func assert(_ condition: @autoclosure () -> Bool, _ message: @autoclosure () -> String = String(), file: StaticString = #file, line: UInt = #line) {
     Swift.assert(condition(), message(), file: file, line: line)
 }
 
-@available(*, message: "Use #assertionFailure instead")
+@available(*, deprecated, message: "Use #assertionFailure instead")
 public func assertionFailure(_ message: @autoclosure () -> String = String(), file: StaticString = #file, line: UInt = #line) {
     Swift.assertionFailure(message(), file: file, line: line)
 }
