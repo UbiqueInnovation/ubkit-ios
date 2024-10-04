4f759842db9d99d9f0843f76f48da6f54895387b
Matthias Felix <felix@ubique.ch>
2024-10-04 18:01:14 +0200

    Make QRScannerView protocol @MainActor

--- Sources/UBQRScanner/QRScannerViewDelegate.swift
+++ Sources/UBQRScanner/QRScannerViewDelegate.swift
@@ -9,6 +9,7 @@ import Foundation
 
 /// Protocol for a delegate that receives events from the `QRScannerView`,
 /// like successfully scanned codes or errors
+@MainActor
 public protocol QRScannerViewDelegate: AnyObject {
     func qrScanningDidFailWithError(_ error: QRScannerError)
     // if true is returned no more codes will be passed to this delegate from the current frame
